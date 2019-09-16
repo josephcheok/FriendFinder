df332d74e7550fe0bc36e20e2bc1c76ae8f02790
josephcheok <joseph@cheokandco.com>
2019-09-16 16:23:11 +1000

    Basic form setup with submission working

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friendArray = [
+  {
+    name: "Jennifer Aniston",
+    photo: "./assets/img/Aniston.jpg",
+    scores: [5, 3, 2, 1, 4, 3, 2, 4, 3, 5]
+  },
+  {
+    name: "Jennifer Connelly",
+    photo: "./assets/img/Connelly.jpg",
+    scores: [5, 2, 2, 2, 4, 3, 2, 4, 3, 5]
+  },
+  {
+    name: "Jennifer Garner",
+    photo: "./assets/img/Garner.jpg",
+    scores: [1, 4, 4, 3, 2, 5, 2, 3, 1, 1]
+  },
+  {
+    name: "Jennifer Hawkins",
+    photo: "./assets/img/Hawkins.jpg",
+    scores: [2, 3, 5, 1, 4, 2, 4, 3, 5, 2]
+  },
+  {
+    name: "Jennifer Lawrence",
+    photo: "./assets/img/Lawrence.jpg",
+    scores: [1, 5, 5, 1, 5, 1, 5, 1, 5, 1]
+  },
+  {
+    name: "Jennifer Lopez",
+    photo: "./assets/img/Lopez.jpg",
+    scores: [2, 3, 4, 2, 3, 4, 2, 3, 3, 4]
+  },
+  {
+    name: "Jennifer Love Hewitt",
+    photo: "./assets/img/Love-Hewitt.jpg",
+    scores: [4, 5, 3, 2, 1, 1, 2, 3, 4, 5]
+  }
+];
+
+module.exports = friendArray;
