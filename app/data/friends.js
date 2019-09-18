a1ef099b1b9a7cbc027d469c0778d9a29eb5166c
josephcheok <joseph@cheokandco.com>
2019-09-19 01:39:57 +1000

    Theming perfected

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,52 +1,59 @@
 var friendArray = [
   {
-    name: "Jennifer Aniston",
+    name: "Aniston",
     photo: "./assets/img/Aniston.jpg",
     scores: [5, 5, 3, 1, 4, 2, 5, 4, 2, 5],
     about:
-      "Coming from multiple failed romances and two broken marriages, this hasn't fazed her outlook on life. At 50 years of age, Jennifer is the fifth highest paid actress in 2019. Experiences make her tougher and she draws from it."
+      "Coming from multiple failed romances and two broken marriages, this hasn't fazed her outlook on life. At 50 years of age, Jennifer is the fifth highest paid actress in 2019. Experiences make her tougher and she draws from it.",
+    online: "10 minutes"
   },
   {
-    name: "Jennifer Connelly",
+    name: "Connelly",
     photo: "./assets/img/Connelly.jpg",
     scores: [3, 2, 4, 4, 3, 1, 3, 4, 1, 2],
     about:
-      "Best Supporting Actress for A Beautiful Mind (2001). Connelly's winning role as a wife supporting her husband suffering from paranoid schizophrenia, wasn't because of her acting ability alone: it was an extension of her. She is a known ambassador for Human Rights Education."
+      "Best Supporting Actress for A Beautiful Mind (2001). Connelly's winning role as a wife supporting her husband suffering from paranoid schizophrenia, wasn't because of her acting ability alone: it was an extension of her.",
+    online: "2 days"
   },
   {
-    name: "Jennifer Garner",
+    name: "Garner",
     photo: "./assets/img/Garner.jpg",
     scores: [2, 3, 5, 5, 4, 4, 5, 2, 1, 3],
     about:
-      "The star of Alias from 2001 to 2006 and the ex-wife to Ben Affleck from 2005 to 2015, she knows what it is like to stay in a long term engagement, to lose it and to never look back."
+      "The star of Alias from 2001 to 2006 and the ex-wife to Ben Affleck from 2005 to 2015, she knows what it is like to stay in a long term engagement, to lose it and to never look back.",
+    online: "3 hours"
   },
   {
-    name: "Jennifer Hawkins",
+    name: "Hawkins",
     photo: "./assets/img/Hawkins.jpg",
     scores: [1, 4, 1, 3, 5, 2, 2, 2, 4, 4],
     about:
-      "The well known face of Myer spring fashion, Jennifer is now married to Jake Wall. But like her role, it may only be for a season."
+      "The well known face of Myer spring fashion, Jennifer is now married to Jake Wall. But like her role, it may only be for a season.",
+    online: "30 minutes"
   },
   {
-    name: "Jennifer Lawrence",
+    name: "Lawrence",
     photo: "./assets/img/Lawrence.jpg",
     scores: [5, 2, 5, 1, 5, 1, 3, 5, 2, 3],
     about:
-      "The star of Hunger Games and Academy Award Winner winner (Silver Linings Playbook), Jennifer Lawrence was a high school dropout from age 14 and is a self-taught actor. If there is one word that defines her, it's determination."
+      "The star of Hunger Games and Academy Award Winner winner (Silver Linings Playbook), Jennifer Lawrence was a high school dropout from age 14 and is a self-taught actor. If there is one word that defines her, it's determination.",
+    online: "1 week"
   },
   {
-    name: "Jennifer Lopez",
+    name: "Lopez",
     photo: "./assets/img/Lopez.jpg",
     scores: [4, 4, 5, 4, 4, 2, 5, 1, 1, 5],
     about:
-      'At 50 years of age, there is no slowing down "Jenny from the Bronx". Still spotting an athletic body and belting out tunes like she was 25, she knows what she wants and is unstopabble.'
+      'At 50 years of age, there is no slowing down "Jenny from the Bronx". Still spotting an athletic body and belting out tunes like she was 25, she knows what she wants and is unstopabble.',
+    online: "12 hours"
   },
   {
-    name: "Jennifer Love Hewitt",
+    name: "Love Hewitt",
     photo: "./assets/img/Love-Hewitt.jpg",
     scores: [1, 1, 3, 2, 2, 3, 4, 3, 3, 1],
     about:
-      "Star of the Client List, a story about a woman forced into a massage parlour to save her house and family from destitution, the show gained critical acclaim for the first season but got shut down after the second. Cited due to a lack of imagination."
+      "Producer and star of the Client List, a story about a woman forced into a massage parlour to save her family from destitution, the show gained critical acclaim for the first season but got shut down after the second. Cited due to a lack of imagination.",
+    online: "1 month"
   }
 ];
 
